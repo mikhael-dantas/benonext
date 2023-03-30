7cda9680d3c845ed53ef4741e9ad6e271b9236b6
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-30 18:05:20 -0300

    setup

--- src/lib/csrf/FetchCsrfToken.ts
+++
@@ -1,9 +0,0 @@
-
-export async function FetchCsrfToken(): Promise<{ csrfToken: string } | {error: string}> {
-    const csrfResponse = await fetch("/api/csrf/get-token", {
-        credentials: "include",
-    })
-    const parsedResponse = await csrfResponse.json()
-
-    return {csrfToken: parsedResponse.csrfToken}
-}
