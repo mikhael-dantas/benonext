d9ed077b30c6ef3ab5b790947ca251d501938fdf
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-30 23:24:56 -0300

    tudo de novo

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -18,7 +18,7 @@ const apiRoute = nextConnect({
 
 apiRoute.get(async (req, res) => {
         res.statusCode = 200;
-        res.write("hello world");
+        res.write("hello worldoooo");
         res.end();
 });
 
